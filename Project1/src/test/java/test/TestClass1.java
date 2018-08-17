fb0fb5a5bd07ccaf43b2e6540461f4e3545cbeca
mujeeb.mohammed <mujeeb.mohammed@HDC3-D-434RPXW.dir.svc.accenture.com>
2018-08-17 10:44:08 +0530

    commit1

---
+++ Project1/src/test/java/test/TestClass1.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+	
+	@Test
+	public void method1() {
+		
+		System.out.println("This is the first method");
+		
+		System.out.println("This is the first method");
+	}
+
+}
